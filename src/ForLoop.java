568126d81e31d765ab4d5e1720ed1067d84b763b
faizanshaikh2100 <64514340+faizanshaikh2100@users.noreply.github.com>
2023-04-20 16:54:35 +0530

    Add files via upload

---
+++ src/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("This is i loop with iteration no :"+i+1);
+            for (int j = 0; j < 10; j++) {
+                System.out.println("This is j loop with iteration no :"+j+1);
+            }
+        }
+    }
+}
