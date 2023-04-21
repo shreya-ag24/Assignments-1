d53eb0fe147b09616e7065cb47607c8f419dc3bd
unknown <faizanshaikh2100@gmail.com>
2023-04-21 10:45:38 +0530

    Created Class clone and added Exceptions

--- src/Day2_CreateClass.java
+++ src/Day2_CreateClass.java
@@ -5,11 +5,24 @@ public class Day2_CreateClass {
         System.out.println("Adding the number..");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // create object with new keyword
         Day2_CreateClass cc = new Day2_CreateClass();
         cc.add();
         System.out.println(cc);
+
+        try {
+            Class c = Class.forName("Day2_CreateClass");
+            Day2_CreateClass c1 = (Day2_CreateClass) c.newInstance();
+            c1.add();
+            System.out.println(c1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Day2_CreateClass objClone = (Day2_CreateClass) cc.clone();
+        objClone.add();
+        System.out.println("objectClone: " + objClone);
     }
 }
