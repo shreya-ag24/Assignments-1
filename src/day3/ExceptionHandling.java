38c690ab02472def984ee64271e29849b24ac2fe
unknown <faizanshaikh2100@gmail.com>
2023-04-24 12:34:49 +0530

    added Exception Handling

---
+++ src/day3/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        System.out.println("First attempt");
+
+        int arr[] = {1,2,3,4};
+        try{
+            System.out.println(arr[1]);
+            System.out.println(arr[4]);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+    }
+
+}
